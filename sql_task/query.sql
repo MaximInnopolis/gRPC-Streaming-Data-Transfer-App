3c4f0cc32f885526256e12f99cc4446f2551505f
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2023-09-29 13:11:05 +0300

    Update query.sql

--- sql_task/query.sql
+++ sql_task/query.sql
@@ -1,3 +1,17 @@
 SELECT surname
 FROM table_name
 ORDER BY (surname = 'Иванов') DESC , surname ASC;
+
+-----------------------------------------------
+
+
+SELECT surname
+FROM (
+         SELECT surname, 1 AS sort_order
+         FROM table_name
+         WHERE surname = 'Иванов'
+         UNION ALL
+         SELECT surname, 2 AS sort_order
+         FROM table_name
+     ) AS sorted_surnames
+ORDER BY sort_order, surname ASC;
